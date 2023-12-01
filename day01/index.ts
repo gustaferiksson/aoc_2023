2c85394dfae82e0f9a5f6d5f6dcaae7b70de800c
Gustaf Eriksson <mail@gustaf.dev>
2023-12-01 07:15:35 +0100

    Day 01 complete 🎄

--- day01/index.ts
+++ day01/index.ts
@@ -1,18 +1,39 @@
 import { file } from 'bun'
 
+const add = (a: number, b: number) => a + b
+const digits = (s: string) =>
+  s
+    .replaceAll('one', 'one1one')
+    .replaceAll('two', 'two2two')
+    .replaceAll('three', 'three3three')
+    .replaceAll('four', 'four4four')
+    .replaceAll('five', 'five5five')
+    .replaceAll('six', 'six6six')
+    .replaceAll('seven', 'seven7seven')
+    .replaceAll('eight', 'eight8eight')
+    .replaceAll('nine', 'nine9nine')
+
 const input = {
   s: await file('./input.txt').text(),
-  get parse() {
-    return this.s.split('\n').map((x) => Number(x))
+  get lines() {
+    return this.s.split('\n')
   },
 }
 
-const add = (a: number, b: number) => a + b
-const mul = (a: number, b: number) => a * b
-
 export const day01 = {
-  part1: () => input.parse.reduce(add),
-  part2: () => input.parse.reduce(mul),
+  part1: () =>
+    input.lines
+      .map((x) => [...x].map(Number).filter((y) => !Number.isNaN(y)))
+      .map((l) => [l.at(0), l.at(-1)].join(''))
+      .map(Number)
+      .reduce(add),
+  part2: () =>
+    input.lines
+      .map(digits)
+      .map((x) => [...x].map(Number).filter((y) => !Number.isNaN(y)))
+      .map((l) => [l.at(0), l.at(-1)].join(''))
+      .map(Number)
+      .reduce(add),
 }
 
 process.env.part === 'part1' && console.log(day01.part1())
