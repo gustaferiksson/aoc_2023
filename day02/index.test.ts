f32353b78fa5684c5158fc7a534f9e206422770b
Gustaf Eriksson <mail@gustaf.dev>
2023-12-02 12:02:11 +0100

    Day 02 complete 🎄

--- day02/index.test.ts
+++ day02/index.test.ts
@@ -2,10 +2,10 @@ import { describe, expect, test } from 'bun:test'
 import { day02 } from './index.ts'
 
 describe('Tests day02', () => {
-  test.todo('Part 1', () => {
-    expect(day02.part1()).toBe(1379)
+  test('Part 1', () => {
+    expect(day02.part1()).toBe(2278)
   })
-  test.todo('Part 2', () => {
-    expect(day02.part2()).toBe(56154)
+  test('Part 2', () => {
+    expect(day02.part2()).toBe(67953)
   })
 })
--- day02/index.ts
+++ day02/index.ts
@@ -1,21 +1,54 @@
 import { file } from 'bun'
 
+type Set = {
+  red?: number
+  green?: number
+  blue?: number
+}
+type Game = { id: number; sets: Set[] }
+
+const add = (a: number, b: number) => a + b
+const mul = (a: number, b: number) => a * b
+
 const input = {
   s: await file('./input.txt').text(),
   get lines() {
     return this.s.split('\n')
   },
-  get numbers() {
-    return this.lines.map(Number)
+  get games(): Game[] {
+    return this.lines.map((l) => {
+      const [a, b] = l.split(': ')
+      const [, id] = a.split(' ').map(Number)
+      const sets = b.split('; ').map((set) =>
+        set.split(', ').reduce((acc, curr) => {
+          const [n, color] = curr.split(' ')
+          acc[color as keyof Set] = +n
+          return acc
+        }, {} as Set),
+      )
+      return { id, sets }
+    })
   },
 }
 
-const add = (a: number, b: number) => a + b
-const mul = (a: number, b: number) => a * b
+const max = { red: 12, green: 13, blue: 14 }
+const isValidSet = (a: Set) => {
+  return (a.red == null || a.red <= max.red) && (a.green == null || a.green <= max.green) && (a.blue == null || a.blue <= max.blue)
+}
+
+const getMaxSet = (sets: Set[]): Set => ({
+  red: sets.reduce((prev, curr) => ((curr.red ?? 0) > (prev.red ?? 0) ? curr : prev), { red: 0 }).red,
+  green: sets.reduce((prev, curr) => ((curr.green ?? 0) > (prev.green ?? 0) ? curr : prev), { green: 0 }).green,
+  blue: sets.reduce((prev, curr) => ((curr.blue ?? 0) > (prev.blue ?? 0) ? curr : prev), { blue: 0 }).blue,
+})
 
 export const day02 = {
-  part1: () => input.numbers.reduce(add),
-  part2: () => input.numbers.reduce(mul),
+  part1: () =>
+    input.games
+      .filter(({ sets }) => sets.every(isValidSet))
+      .map(({ id }) => id)
+      .reduce(add),
+  part2: () => input.games.map(({ sets }) => Object.values(getMaxSet(sets)).reduce(mul)).reduce(add),
 }
 
 process.env.part === 'part1' && console.log(day02.part1())
