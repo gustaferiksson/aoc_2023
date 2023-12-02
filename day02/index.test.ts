dca5f0fe1e7e96a0389fb827f77d83ac383ff845
Gustaf Eriksson <mail@gustaf.dev>
2023-12-02 10:08:36 +0100

    Setup template for day02

---
+++ day02/index.test.ts
@@ -0,0 +1,11 @@
+import { describe, expect, test } from 'bun:test'
+import { day02 } from './index.ts'
+
+describe('Tests day02', () => {
+  test.todo('Part 1', () => {
+    expect(day02.part1()).toBe(1379)
+  })
+  test.todo('Part 2', () => {
+    expect(day02.part2()).toBe(56154)
+  })
+})
