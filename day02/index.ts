16faa513ce9352b1dc6676ca84a8c30db4a40560
Gustaf Eriksson <mail@gustaf.dev>
2023-12-02 12:44:09 +0100

    Day 02 with regex instead 🎄

--- day02/index.ts
+++ day02/index.ts
@@ -1,14 +1,8 @@
 import { file } from 'bun'
 
-type Set = {
-  red?: number
-  green?: number
-  blue?: number
-}
-type Game = { id: number; sets: Set[] }
+type Game = { id: number; red: number[]; green: number[]; blue: number[] }
 
 const add = (a: number, b: number) => a + b
-const mul = (a: number, b: number) => a * b
 
 const input = {
   s: await file('./input.txt').text(),
@@ -16,39 +10,25 @@ const input = {
     return this.s.split('\n')
   },
   get games(): Game[] {
-    return this.lines.map((l) => {
-      const [a, b] = l.split(': ')
-      const [, id] = a.split(' ').map(Number)
-      const sets = b.split('; ').map((set) =>
-        set.split(', ').reduce((acc, curr) => {
-          const [n, color] = curr.split(' ')
-          acc[color as keyof Set] = +n
-          return acc
-        }, {} as Set),
-      )
-      return { id, sets }
-    })
+    return this.lines.map((l) => ({
+      id: Number(l.split(':').at(0)?.split(' ').at(-1)),
+      red: l.match(/(\d+)(?= red)/g)?.map(Number) ?? [],
+      green: l.match(/(\d+)(?= green)/g)?.map(Number) ?? [],
+      blue: l.match(/(\d+)(?= blue)/g)?.map(Number) ?? [],
+    }))
   },
 }
 
-const max = { red: 12, green: 13, blue: 14 }
-const isValidSet = (a: Set) => {
-  return (a.red == null || a.red <= max.red) && (a.green == null || a.green <= max.green) && (a.blue == null || a.blue <= max.blue)
-}
-
-const getMaxSet = (sets: Set[]): Set => ({
-  red: sets.reduce((prev, curr) => ((curr.red ?? 0) > (prev.red ?? 0) ? curr : prev), { red: 0 }).red,
-  green: sets.reduce((prev, curr) => ((curr.green ?? 0) > (prev.green ?? 0) ? curr : prev), { green: 0 }).green,
-  blue: sets.reduce((prev, curr) => ((curr.blue ?? 0) > (prev.blue ?? 0) ? curr : prev), { blue: 0 }).blue,
-})
+const validGame = (game: Game) => Math.max(...game.red) <= 12 && Math.max(...game.green) <= 13 && Math.max(...game.blue) <= 14
+const getGamePower = (game: Game) => Math.max(...game.red) * Math.max(...game.green) * Math.max(...game.blue)
 
 export const day02 = {
   part1: () =>
     input.games
-      .filter(({ sets }) => sets.every(isValidSet))
-      .map(({ id }) => id)
+      .filter(validGame)
+      .map((g) => g.id)
       .reduce(add),
-  part2: () => input.games.map(({ sets }) => Object.values(getMaxSet(sets)).reduce(mul)).reduce(add),
+  part2: () => input.games.map(getGamePower).reduce(add),
 }
 
 process.env.part === 'part1' && console.log(day02.part1())
