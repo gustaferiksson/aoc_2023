c1314661d7b4fe911c22433e24e715043fe8d673
Gustaf Eriksson <mail@gustaf.dev>
2023-12-05 18:24:29 +0100

    Day 04 complete 🎄

--- day04/index.test.ts
+++ day04/index.test.ts
@@ -5,7 +5,7 @@ describe('Tests day04', () => {
   test('Part 1', () => {
     expect(day04.part1()).toBe(25004)
   })
-  test.todo('Part 2', () => {
-    expect(day04.part2()).toBe(56154)
+  test('Part 2', () => {
+    expect(day04.part2()).toBe(14427616)
   })
 })
--- day04/index.ts
+++ day04/index.ts
@@ -5,7 +5,7 @@ const input = {
   get lines() {
     return this.s.split('\n')
   },
-  get cards() {
+  get cardMatches() {
     return this.lines.map((line) => line.match(/\d+/g)?.slice(1) ?? []).map((l) => l.length - new Set(l).size)
   },
 }
@@ -15,13 +15,23 @@ const add = (a: number, b: number) => a + b
 export const day04 = {
   // 13
   part1: () =>
-    input.cards
+    input.cardMatches
       .filter((n) => n > 0)
       .map((x) => 2 ** (x - 1))
       .reduce(add, 0),
   // 30
   part2: () => {
-    process.exit(1)
+    const matches = input.cardMatches
+    const copies = Array(matches.length).fill(0)
+
+    for (const [i, m] of matches.entries()) {
+      copies[i] += 1
+      for (const [j, _] of Array(m).entries()) {
+        if (i + j < matches.length) copies[j + i + 1] += copies[i]
+      }
+    }
+
+    return copies.reduce(add)
   },
 }
 
