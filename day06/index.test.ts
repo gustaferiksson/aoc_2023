7e2de5e0d7c1e81d8dba6f70bcebb17804007d1c
Gustaf Eriksson <mail@gustaf.dev>
2023-12-12 17:36:33 +0100

    Day 06 complete 🎄

--- day06/index.test.ts
+++ day06/index.test.ts
@@ -2,10 +2,10 @@ import { describe, expect, test } from 'bun:test'
 import { day06 } from './index.ts'
 
 describe('Tests day06', () => {
-  test.todo('Part 1', () => {
-    expect(day06.part1()).toBe(1379)
+  test('Part 1', () => {
+    expect(day06.part1()).toBe(131376)
   })
-  test.todo('Part 2', () => {
-    expect(day06.part2()).toBe(56154)
+  test('Part 2', () => {
+    expect(day06.part2()).toBe(34123437)
   })
 })
--- day06/index.ts
+++ day06/index.ts
@@ -1,21 +1,39 @@
 import { file } from 'bun'
 
+type Race = { time: number; distance: number }
+
 const input = {
   s: await file('./input.txt').text(),
   get lines() {
     return this.s.split('\n')
   },
-  get numbers() {
-    return this.lines.map(Number)
+  get races() {
+    const times = this.lines[0].match(/\d+/g)?.map(Number)
+    const distances = this.lines[1].match(/\d+/g)?.map(Number)
+
+    return times?.map((time, i) => ({ time, distance: distances?.[i] ?? -1 }))
   },
+  get race() {
+    const time = Number(this.lines[0].match(/\d+/g)?.join(''))
+    const distance = Number(this.lines[1].match(/\d+/g)?.join(''))
+    return { time, distance }
+  },
+}
+
+// D = n * (T - n)
+const wins = ({ time, distance }: Race) => {
+  const list = Array(time)
+  const first = list.findIndex((_, i) => i * (time - i) > distance)
+  const last = list.findLastIndex((_, i) => i * (time - i) > distance)
+  return last - first + 1
 }
 
 const add = (a: number, b: number) => a + b
 const mul = (a: number, b: number) => a * b
 
 export const day06 = {
-  part1: () => input.numbers.reduce(add),
-  part2: () => input.numbers.reduce(mul),
+  part1: () => input.races?.map(wins).reduce(mul),
+  part2: () => wins(input.race),
 }
 
 process.env.part === 'part1' && console.log(day06.part1())
